caedbba2b5f05f73566f793446a0b5747a4d09ea
akaur091 <amniderkaur.09@gmail.com>
2022-04-11 22:44:24 -0700

    add button and link APIs

--- assets/script.js
+++ assets/script.js
@@ -16,14 +16,47 @@
 
 //     * Uses `localStorage` to store persistent data.
 var APIkey= "346fa36c7fb173393b353dd518347322"
-var query= "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid="+ APIkey;
+var queryone= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid="+ APIkey;
+var querylocation= "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&units=imperial&appid="+ APIkey;
 
 $.ajax({
-    url: query,
+    url: queryone,
     method: "GET"
 }).then(function(response) {
 
-    console.log(query);
+    console.log("current data ");
     console.log(response);
 
 });
+
+$.ajax({
+    url:querylocation,
+    method: "GET"
+}).then(function(extenddata){
+    console.log("location data ");
+    console.log(extenddata);
+})
+$(".citybtn").on("click",function(button){
+    button.preventDefault();
+    createQuery();
+    var input =$("#city").val();
+    var cityArrray= JSON.parse(localStorage.getItem("input")) || [];
+    cityArrray.push(input);
+    localStorage.setItem("input", JSON.stringify(cityArrray));
+})
+var weatherimg = extenddata.current.weather[0].icon;
+var img ="https://openweathermap.org/img/wn/"+ weatherimg + ".png";
+
+$(".reportColumn").html("");
+
+$(".reportColumn").append(
+    '<div class="currentForecast"></div>'
+);
+
+$(".currentForecast").append(
+    `<h2 class="atmCity">
+        ${data.name} <span class="atmDate">(${
+        moment.unix(extenddata?.current?.dt)
+        .format("MM/DD/YYYY")})</span> <img id ="weatherimg" src="${img}"/>
+    </h2>`
+);
